922d52e1cd401be9633d3e80d7a6dd7547dbbb30
Gipulina <georgi_palavecino@hotmail.com>
2021-08-17 12:39:18 -0300

    My First commit

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
+// Uso esto para chequear que esté conectada la hoja JS al HTML console.log('testing'); 
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// Prompt to select media stream, pass to video element, then play
-async function selectMediaStream() {
-  try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-    videoElement.srcObject = mediaStream;
-    videoElement.onloadedmetadata = () => {
-      videoElement.play();
-    };
-  } catch (error) {
-    // Catch Error Here
-  }
+//Prompt to select  media stream, pass to video element, then play
+async function selectMediaStream(){
+    try {
+        const MediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = MediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error){
+        // Catch error Here.
+        console.log('whoops, error here:',error);
+    }
 }
+button.addEventListener('click',async () => {
+    // Disable button
+    button.disabled = true;
+    //Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    //Resert Button
+    button.disabled = false;
 
-button.addEventListener('click', async () => {
-  // Disable Button
-  button.disabled = true;
-  // Start Picture in Picture
-  await videoElement.requestPictureInPicture();
-  // Reset Button
-  button.disabled = false;
 });
-
-// On Load
+//On Load
 selectMediaStream();
